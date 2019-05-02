7985b80c1cb3c7de6ddd9d2ee928e6bf97c81168
Shravan Zolgiker <M1047488@mindtree.com>
2019-05-02 13:28:09 +0530

    added db config, routes file

---
+++ routes/api/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// @route GET api/user
+// @desc  Test route
+// @access Public
+router.get("/", (req, res) => res.send("user Route"));
+
+module.exports = router;
