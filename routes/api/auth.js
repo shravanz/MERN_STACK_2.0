e561dbd2a8e39eac70111f0dec9c4b0222f25417
Shravan Zolgiker <M1047488@mindtree.com>
2019-05-03 14:38:11 +0530

    added login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+
 const auth = require("../../middleware/auth");
 
 const User = require("../../models/User");
@@ -16,5 +21,53 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route POST api/auth
+// @desc  Authenticate user and get token
+// @access Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      //see user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: [{ msg: "User not Found" }] });
+      }
+      //Match the User cerdentials
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ error: [{ msg: "User not Found" }] });
+      }
+      // return jwt
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
