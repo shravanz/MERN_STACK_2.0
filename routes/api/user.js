54a2b2dd3d5a39d4bdec05c2d6202d7fc3fc26b4
Shravan Zolgiker <M1047488@mindtree.com>
2019-05-03 13:57:17 +0530

    added jwt middlware

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const { check, validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../../models/User");
 
@@ -57,7 +59,20 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
       // return jwt
-      res.send("user Registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
